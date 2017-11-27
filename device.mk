ae988a818bd91746809da1b847637c3322c1d1d7
Ankit <yankit293@gmail.com>
2017-11-27 15:38:56 +0000

    Added stuff

--- device.mk
+++ device.mk
@@ -23,3 +23,7 @@ include $(LOCAL_PATH)/product/*.mk
 
 $(call inherit-product-if-exists, vendor/lenovo/A6020/A6020-vendor.mk)
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbt-vendor \
+    android.hardware.bluetooth@1.0-impl
